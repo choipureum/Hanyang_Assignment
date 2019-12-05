21fbb1e0a4e99c19a6bff8c7fecf306794d00698
최푸름 <poo1994@hanmail.net>
2019-12-05 13:42:37 +0900

    Python_turtle

---
+++ Python_basic/turtle()_1(Olympic_flag).py
@@ -0,0 +1,19 @@
+import turtle
+
+t = turtle.Turtle()
+t.pensize(6)
+firstRowColors = ["blue", "black", "red"]
+for i in range(3):
+  t.penup()
+  t.pencolor(firstRowColors[i])
+  t.goto(i*110, 0)
+  t.pendown()
+  t.circle(50)
+
+secondRowColors = ["", "yellow", "", "green"]
+for i in range(1, 4, 2):
+  t.penup()
+  t.pencolor(secondRowColors[i])
+  t.goto(i*55, -50)
+  t.pendown()
+  t.circle(50)
